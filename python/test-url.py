ccba7b2ec1935f47227ef2542901f0830c2e2a87
malekannabi <60226962+malekannabi@users.noreply.github.com>
2020-07-24 09:02:03 +0100

    chnage of plan

--- python/test-url.py
+++ python/test-url.py
@@ -3,9 +3,9 @@ sj=open('E:\stage2020\Sample.json',"r",encoding="utf-8")
 s=json.load(sj)
 print(s)
 sj.close()
-href=[]
+coor=[]
 for i in s['briefs']:
     if i['cnxState']=='DISC' and i['healthState']=='BACKHAUL_CNX_ERROR':
-        href.append(i['href'])
-print("href :  ")
-print(href)
+        coor.append([i['lat'],i['lon']])
+print("coor :  ")
+print(coor)
